d180dd087da66b93a1eb183340e847b8ff94aa6f
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2023-08-30 09:50:17 +0200

    feat: add progress header bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,24 @@
-import '../styles/globals.css'
-import { Nunito_Sans } from 'next/font/google'
-import { ThemeProvider } from 'next-themes'
+import "../styles/globals.css";
+import { Nunito_Sans } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 
 const nunito = Nunito_Sans({
-  subsets: ['latin'],
-  variable: '--font-nunito-sans',
-})
+  subsets: ["latin"],
+  variable: "--font-nunito-sans",
+});
 
-import { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import { RouteProgress } from "@/components/RouteProgress";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <div className={nunito.className}>
+        <RouteProgress />
         <Component {...pageProps} />
       </div>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
