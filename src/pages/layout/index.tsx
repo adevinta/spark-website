f391c55d9908b48242d04b59dd9e2c8e21717eec
andresin87 <andresin87@gmail.com>
2023-09-01 09:38:24 +0200

    FIX warnings and layout header-footer width

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -4,17 +4,29 @@ export default function IndexPage() {
   return (
     <Layout>
       <Layout.TopBanner>
-        <div className="bg-background" contentEditable>
+        <div
+          className="bg-background"
+          contentEditable
+          suppressContentEditableWarning
+        >
           top banner
         </div>
       </Layout.TopBanner>
       <Layout.Header>
-        <header className="bg-background-variant" contentEditable>
+        <header
+          className="bg-background-variant"
+          contentEditable
+          suppressContentEditableWarning
+        >
           header
         </header>
       </Layout.Header>
       <Layout.Hero>
-        <div className="bg-basic text-on-basic" contentEditable>
+        <div
+          className="bg-basic text-on-basic"
+          contentEditable
+          suppressContentEditableWarning
+        >
           hero
         </div>
       </Layout.Hero>
@@ -22,22 +34,35 @@ export default function IndexPage() {
         <div
           className="bg-basic-container text-on-basic-container"
           contentEditable
+          suppressContentEditableWarning
         >
           leading panel
         </div>
       </Layout.LeadingPanel>
       <Layout.LeadingSidebar>
-        <div className="bg-main-variant text-on-main-variant" contentEditable>
+        <div
+          className="bg-main-variant text-on-main-variant"
+          contentEditable
+          suppressContentEditableWarning
+        >
           leading sidebar
         </div>
       </Layout.LeadingSidebar>
       <Layout.Content>
-        <div className="bg-main text-on-main" contentEditable>
+        <div
+          className="bg-main text-on-main"
+          contentEditable
+          suppressContentEditableWarning
+        >
           content
         </div>
       </Layout.Content>
       <Layout.TrailingSidebar>
-        <div className="bg-main-variant text-on-main-variant" contentEditable>
+        <div
+          className="bg-main-variant text-on-main-variant"
+          contentEditable
+          suppressContentEditableWarning
+        >
           trailing sidebar
         </div>
       </Layout.TrailingSidebar>
@@ -45,17 +70,26 @@ export default function IndexPage() {
         <div
           className="bg-basic-container text-on-basic-container"
           contentEditable
+          suppressContentEditableWarning
         >
           trailing panel
         </div>
       </Layout.TrailingPanel>
       <Layout.Footer>
-        <footer className="bg-background-variant" contentEditable>
+        <footer
+          className="bg-background-variant"
+          contentEditable
+          suppressContentEditableWarning
+        >
           footer
         </footer>
       </Layout.Footer>
       <Layout.BottomBanner>
-        <div className="bg-background" contentEditable>
+        <div
+          className="bg-background"
+          contentEditable
+          suppressContentEditableWarning
+        >
           bottom banner
         </div>
       </Layout.BottomBanner>
