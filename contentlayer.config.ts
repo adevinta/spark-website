604e943fef4d32c9218adafb0e5067f877b313e9
Powerplex <julien.bonnin.dev@gmail.com>
2023-09-04 14:09:43 +0200

    setup docgen in contentLayer

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -2,7 +2,39 @@ import { defineDocumentType, makeSource } from "contentlayer/source-files";
 import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import * as fs from "fs";
+import * as path from "path";
 import * as examples from "./src/examples";
+
+function readDocgenFiles() {
+  const nodeModulesDir = path.resolve(
+    new URL(import.meta.url).pathname,
+    "../../../../node_modules/@spark-ui"
+  );
+
+  const packagesDir = fs.readdirSync(nodeModulesDir);
+  const docgenContents: Record<string, string | null> = {};
+
+  for (const packageName of packagesDir) {
+    const docgenFilePath = path.join(
+      nodeModulesDir,
+      packageName,
+      "dist/public/docgen.json"
+    );
+
+    try {
+      const content = fs.readFileSync(docgenFilePath, "utf-8");
+      docgenContents[packageName] = JSON.parse(content);
+    } catch (err) {
+      // Handle cases where the file doesn't exist
+      docgenContents[packageName] = null;
+    }
+  }
+
+  return docgenContents;
+}
+
+const docgenData = readDocgenFiles();
 
 export const Doc = defineDocumentType(() => ({
   name: "Doc",
@@ -30,6 +62,14 @@ export const Doc = defineDocumentType(() => ({
       resolve: (doc) => {
         const slug = doc._raw.sourceFileName.replace(/\.mdx$/, "");
         return slug;
+      },
+    },
+    docgen: {
+      type: "json",
+      resolve: (doc) => {
+        const packageName = doc._raw.sourceFileName.replace(/\.mdx$/, "");
+
+        return docgenData[packageName];
       },
     },
     examples: {
