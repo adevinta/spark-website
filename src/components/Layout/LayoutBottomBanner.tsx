04c14d42b8c95e334e5f5ff6290637b448ba0a37
andresin87 <andresin87@gmail.com>
2023-09-04 10:10:37 +0200

    remove: remove unnecessary ts comment

--- src/components/Layout/LayoutBottomBanner.tsx
+++ src/components/Layout/LayoutBottomBanner.tsx
@@ -4,7 +4,6 @@ import { cx } from 'class-variance-authority'
 import { LayoutBanner, type LayoutBannerProps } from './LayoutBanner'
 
 export type LayoutBottomBannerProps = LayoutBannerProps
-// @ts-ignore
 export const LayoutBottomBanner = forwardRef<HTMLDivElement, LayoutBottomBannerProps>(
   ({ children, className, ...props }, forwardedRef) => (
     <LayoutBanner ref={forwardedRef} className={cx('w-screen grow-0', className)} {...props}>
