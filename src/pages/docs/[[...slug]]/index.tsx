39de4ef3f5e02ba02a70ca5c13e71499190f3d95
andresin87 <andresin87@gmail.com>
2023-09-06 15:33:39 +0200

    fix(type): pre,next typed

--- src/pages/docs/[[...slug]]/index.tsx
+++ src/pages/docs/[[...slug]]/index.tsx
@@ -23,7 +23,7 @@ const DocsDetailPage = ({ doc }: DocsDetailPageProps) => {
 
       <LayoutHeader />
 
-      <LayoutContainer className="flex w-full gap-2xl min-h-[calc(100dvh-var(--sz-64))]">
+      <LayoutContainer className="flex min-h-[calc(100dvh-var(--sz-64))] w-full gap-2xl">
         <LayoutSideNav />
 
         <main className="flex w-full flex-row gap-2xl">
